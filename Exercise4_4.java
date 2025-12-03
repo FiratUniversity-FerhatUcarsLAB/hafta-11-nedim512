3fa2d90a56ec84a094942e824bcafdeb6953fd16
nedim512 <nedii3m@gmail.com>
2025-12-03 23:49:30 +0300

    Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,8 @@
+/*
+ 250541621
+ nedim isa
+ 03.12.2025
+ */
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -11,11 +16,12 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        //Cevap1: Java'da bu hata vermez. Metot çalışır, işini yapar, dönen değer (return value) hafızada bir yere atanmadığı için kaybolur (ignore edilir).
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
+        //Cevap2: Java derleme hatası (compile-time error) verir. Çünkü void metotlar bir değer üretmez, dolayısıyla matematiksel veya mantıksal bir işleme sokulamazlar.
+        
     }
 }
